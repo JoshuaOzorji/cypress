e7ea12487fef3b4592735e6929e86d9f633e3c96
Joshua Ozorji <ozorjijoshua@gmail.com>
2024-10-20 15:05:46 +0100

    e2e test wrap

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,9 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+	defaultCommandTimeout: 10000,
+	pageLoadTimeout: 60000,
+
 	e2e: {
 		baseUrl: "http://localhost:3000",
 		setupNodeEvents(on, config) {
