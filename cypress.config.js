21311647136f56dfb4569e56a64a0e7218fc40d3
Joshua Ozorji <ozorjijoshua@gmail.com>
2024-10-21 11:30:35 +0100

    components testing

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,20 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-	defaultCommandTimeout: 10000,
-	pageLoadTimeout: 60000,
+  defaultCommandTimeout: 10000,
+  pageLoadTimeout: 60000,
 
-	e2e: {
-		baseUrl: "http://localhost:3000",
-		setupNodeEvents(on, config) {
-			// implement node event listeners here
-		},
-	},
+  e2e: {
+    baseUrl: "http://localhost:3000",
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "next",
+      bundler: "webpack",
+    },
+  },
 });
